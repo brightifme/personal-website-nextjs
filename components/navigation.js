602e46a2c9b306d05ad3ba5031d77a93c08816e4
Bright Ibeme <jjohn1568@gmail.com>
2022-07-05 20:58:09 +0100

    first commit

---
+++ components/navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <div className="navigation">
+   
+
+      <main >
+        <h1>
+          Navigation
+        </h1>
+
+      </main>
+    </div>
+  )
+}
